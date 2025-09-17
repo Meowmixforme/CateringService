5d45a9ca97ddb74bdd7e4500915674049b521374
Meowmixforme <v8255920@live.tees.ac.uk>
2025-09-17 03:12:36 +0100

    Backend completed

---
+++ ThAmCo.Catering/Migrations/20250917020638_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThAmCo.Catering.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuFoodItems_FoodItems_FoodItemId",
+                table: "MenuFoodItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuFoodItems_Menus_MenuId",
+                table: "MenuFoodItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuFoodItems_FoodItems_FoodItemId",
+                table: "MenuFoodItems",
+                column: "FoodItemId",
+                principalTable: "FoodItems",
+                principalColumn: "FoodItemId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuFoodItems_Menus_MenuId",
+                table: "MenuFoodItems",
+                column: "MenuId",
+                principalTable: "Menus",
+                principalColumn: "MenuId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuFoodItems_FoodItems_FoodItemId",
+                table: "MenuFoodItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuFoodItems_Menus_MenuId",
+                table: "MenuFoodItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuFoodItems_FoodItems_FoodItemId",
+                table: "MenuFoodItems",
+                column: "FoodItemId",
+                principalTable: "FoodItems",
+                principalColumn: "FoodItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuFoodItems_Menus_MenuId",
+                table: "MenuFoodItems",
+                column: "MenuId",
+                principalTable: "Menus",
+                principalColumn: "MenuId");
+        }
+    }
+}
