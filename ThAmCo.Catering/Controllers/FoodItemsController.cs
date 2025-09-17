242fd709eb5135e13322566e847d84102336aa37
Meowmixforme <v8255920@live.tees.ac.uk>
2025-09-17 02:48:02 +0100

    Refactor FoodItemsController to use DTOs and improve API consistency
    - Updated all API responses to return FoodItemDTO instead of EF entities
    - Removed unnecessary null checks for lists; ToListAsync always returns a list
    - In POST, return DTO instead of entity after creation
    - In PUT, add NotFound check and return NoContent on success
    - Changed DELETE to return IActionResult with NoContent
    - Cleaned up parameter naming and improved code readability

--- ThAmCo.Catering/Controllers/FoodItemsController.cs
+++ ThAmCo.Catering/Controllers/FoodItemsController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ThAmCo.Catering.Data;
@@ -26,111 +23,90 @@ namespace ThAmCo.Catering.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<FoodItemDTO>>> GetFoodItems()
         {
-
-
-            var Fooditem = await _context
-                .FoodItems
-
-                //Project them into the DTO shape
+            var foodItems = await _context.FoodItems
                 .Select(f => new FoodItemDTO
                 {
                     FoodItemId = f.FoodItemId,
                     Description = f.Description,
-                    UnitPrice = f.UnitPrice,
-                    
-                    
-
-
+                    UnitPrice = f.UnitPrice
                 })
                 .ToListAsync();
 
-            if (GetFoodItems == null)
-            {
-                return NotFound();
-            }
-            return Ok(Fooditem);
+            return Ok(foodItems);
         }
 
         // GET: api/FoodItems/5
         [HttpGet("{id}")]
         public async Task<ActionResult<FoodItemDTO>> GetFoodItem(int id)
         {
-
-            var foodItem = await _context
-            .FoodItems
-            .FirstOrDefaultAsync(m => m.FoodItemId == id);
+            var foodItem = await _context.FoodItems
+                .FirstOrDefaultAsync(m => m.FoodItemId == id);
 
             if (foodItem == null)
             {
                 return NotFound();
             }
 
-            //Project them into the DTO shape
-            ThAmCo.Catering.DTOs.FoodItemDTO dto = new DTOs.FoodItemDTO();
-            dto.FoodItemId = id;
-            dto.Description = foodItem.Description;
-            dto.UnitPrice = foodItem.UnitPrice;
-            
+            var dto = new FoodItemDTO
+            {
+                FoodItemId = foodItem.FoodItemId,
+                Description = foodItem.Description,
+                UnitPrice = foodItem.UnitPrice
+            };
 
-
-
-            // return them
             return Ok(dto);
-
         }
 
         // PUT: api/FoodItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<ActionResult<FoodItemDTO>> PutFoodItem(int id, FoodItemDTO FoodItemDTO)
-
+        public async Task<IActionResult> PutFoodItem(int id, FoodItemDTO foodItemDto)
         {
-
-            if (id != FoodItemDTO.FoodItemId)
+            if (id != foodItemDto.FoodItemId)
             {
                 return BadRequest();
             }
 
-            var foodItem = await _context
-            .FoodItems
-            .FirstOrDefaultAsync(m => m.FoodItemId == id);
-            //Project them into the entity to server from DTO
+            var foodItem = await _context.FoodItems.FindAsync(id);
+            if (foodItem == null)
             {
-                foodItem.Description = FoodItemDTO.Description;
-                foodItem.UnitPrice = FoodItemDTO.UnitPrice!;
-                
-            };
+                return NotFound();
+            }
 
+            foodItem.Description = foodItemDto.Description;
+            foodItem.UnitPrice = foodItemDto.UnitPrice;
 
             await _context.SaveChangesAsync();
 
-            return Ok();
+            return NoContent();
         }
 
-
         // POST: api/FoodItems
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<FoodItemDTO>> PostFoodItem(FoodItemDTO createFoodItem)
-
-        {   //Project them into the entity to server from DTO
+        {
             var foodItem = new FoodItem
             {
                 Description = createFoodItem.Description,
-                UnitPrice = createFoodItem.UnitPrice,
-
+                UnitPrice = createFoodItem.UnitPrice
             };
 
             _context.FoodItems.Add(foodItem);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetFoodItem", new { id = foodItem.FoodItemId }, foodItem);
+            var dto = new FoodItemDTO
+            {
+                FoodItemId = foodItem.FoodItemId,
+                Description = foodItem.Description,
+                UnitPrice = foodItem.UnitPrice
+            };
+
+            return CreatedAtAction(nameof(GetFoodItem), new { id = foodItem.FoodItemId }, dto);
         }
 
         // DELETE: api/FoodItems/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<MenuDTO>> DeleteFoodItem(int id)
-        {   
+        public async Task<IActionResult> DeleteFoodItem(int id)
+        {
             var foodItem = await _context.FoodItems.FindAsync(id);
             if (foodItem == null)
             {
